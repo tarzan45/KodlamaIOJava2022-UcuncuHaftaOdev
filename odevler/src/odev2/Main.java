294e0228b48027b366069692a8fc8b42fe531b34
tarzan45 <lutfullahkaynak45@hotmail.com>
2022-10-17 22:20:00 +0300

    Update

--- odevler/src/odev2/Main.java
+++ odevler/src/odev2/Main.java
@@ -3,7 +3,20 @@ package odev2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GetterAndSetter product=new GetterAndSetter();
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus Laptop");
+		product.setPrice(50000);
+		product.setStockAmount(3);
+		
+		
+		ProductManager productManager=new ProductManager();
+		productManager.Add(product);
+		
+		System.out.println(product.getKod());
+		
+		
 
 	}
 
---
+++ odevler/src/odev2/ProductManager.java
@@ -0,0 +1,9 @@
+package odev2;
+
+public class ProductManager {
+
+	public void Add(GetterAndSetter g)
+	{
+		System.out.println("Ürün eklendi "+ g.getName());
+	}
+}
